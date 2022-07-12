5208e6234a15e8d5f1613a48374099fbc01bb4b4
Christian Skorobogatow <christiansko@icloud.com>
2022-07-12 18:02:02 +0200

    Initial Commit

---
+++ GeometryReader/GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderApp.swift
+//  GeometryReader
+//
+//  Created by Christian Skorobogatow on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
